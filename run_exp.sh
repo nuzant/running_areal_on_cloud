ee66a6c680df5815ca4ceb4250b03dd20862b420
晓雷 <meizhiyu.mzy@antgroup.com>
2025-08-01 21:54:57 +0800

    .

--- run_exp.sh
+++ run_exp.sh
@@ -4,4 +4,4 @@ cmd="python3 -m arealite.launcher.ray scripts/partial_50_25_grpo.py --config scr
      experiment_name=mzy-test trial_name=run0 cluster.n_nodes=4 cluster.n_gpus_per_node=8 allocation_mode=sglang.d16p1t1+d16p1t1"
 
 docker exec raycluster-root ray status
-# docker exec raycluster-user bash -c "cd /AReaL; $cmd"
+# docker exec raycluster-root bash -c "cd /AReaL; $cmd"
